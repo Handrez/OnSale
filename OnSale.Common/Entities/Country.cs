4240c3eb934d98d80262fdb494b810c8b6a65ad8
Andres Giraldo Perez <andresgiraldo249975@correo.itm.edu.co>
2020-08-13 15:24:03 -0500

    Improve countries CRUD

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Security.Principal;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The filed {0} must contain less than {1} characteres.")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
