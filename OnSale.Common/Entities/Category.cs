9b978e3f3ec59eb701083f6f4b3ae1430de5edee
Andres Giraldo Perez <andresgiraldo249975@correo.itm.edu.co>
2020-08-15 20:32:06 -0500

    Complete categories CRUD

---
+++ OnSale.Common/Entities/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnSale.Common.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The filed {0} must contain less than {1} characteres.")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Image")]
+        public Guid ImageId { get; set; }
+
+        //TODO: Pending to put the correct paths
+        [Display(Name = "Image")]
+        public string ImageFullPath => ImageId == Guid.Empty
+            ? $"https://localhost:44329/images/noimage.png"
+            : $"https://handrez.blob.core.windows.net/categories/{ImageId}";
+    }
+}
