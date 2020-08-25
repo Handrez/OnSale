791cfcd46bc36705d73b6948e40f1c356c879b74
Andres Giraldo Perez <andresgiraldo249975@correo.itm.edu.co>
2020-08-25 14:27:50 -0500

    Add Reset Password

--- OnSale.Web/Models/EditUserViewModel.cs
+++ OnSale.Web/Models/EditUserViewModel.cs
@@ -36,8 +36,8 @@ namespace OnSale.Web.Models
 
         [Display(Name = "Image")]
         public string ImageFullPath => ImageId == Guid.Empty
-            ? $"https://OnSaleweb.azurewebsites.net/images/noimage.png"
-            : $"https://onsale.blob.core.windows.net/users/{ImageId}";
+            ? $"https://onsalehandrez.azurewebsites.net/images/noimage.png"
+            : $"https://handrez.blob.core.windows.net/users/{ImageId}";
 
         [Display(Name = "Image")]
         public IFormFile ImageFile { get; set; }
